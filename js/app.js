33ce1083c34a7b48e1c47592089b6586aecbb470
torisd560 <mitalisd123@gmail.com>
2021-09-13 06:22:14 +0600

    added all new styles

--- js/app.js
+++ js/app.js
@@ -16,17 +16,16 @@ const showProducts = (products) => {
      console.log(product)
     const image = product.image;
     const div = document.createElement("div");
-    div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h4>${product.title}</h4>
       <p>Category: ${product.category}</p>
       <h4><span class = "rating">Rate</span> : ${product.rating.rate}</h4>
       <h4><span class = "rating">Count</span>  : ${product.rating.count}</h4>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      <h3>Price: $ ${product.price}</h3>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
