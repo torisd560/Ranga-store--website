28ee295215ed9fdea7bb276622506c79bbf393b1
torisd560 <mitalisd123@gmail.com>
2021-09-13 13:36:49 +0600

    show details

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// load products from API
+//=============== load products from API====================
 const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
@@ -7,28 +7,29 @@ const loadProducts = () => {
 };
 loadProducts();
 
-// show all product in UI 
+//============== show all product in UI ====================
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     const image = product.image;
     const div = document.createElement("div");
-    div.innerHTML = `<div class="single-product">
-      <div>
+    div.classList.add('single-product')
+    div.innerHTML = `
     <img class="product-image" src=${image}></img>
-      </div>
+      <div class = "mt-5">
       <h4>${product.title}</h4>
       <p>Category: ${product.category}</p>
       <p class = "fw-bold"><span class = "text-danger"><i class="fas fa-star"></i> Average Rate</span> : ${product?.rating?.rate}</p>
       <p class = "fw-bold"><span class = "text-success"><i class="fas fa-male"></i> Count</span> : ${product?.rating?.count}</p>
       <h3>Price: $ ${product.price}</h3>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary">Add to cart</button>
-      <button id="details-btn" class="btn btn-secondary">Details</button></div>
+      <button onclick = "loadDetails(${product.id})" id="details-btn" class="btn btn-secondary">Details</button>
+      </div>
       `;
-    document.getElementById("all-products").appendChild(div);
+    document.getElementById("all-products").appendChild(div)
   }
 };
-// add products to cart
+//===================== add products to cart=========================
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -37,14 +38,14 @@ const addToCart = (id, price) => {
   updateTotal()
   document.getElementById("total-Products").innerText = count;
 };
-// common function for innertext
+//================ common function for innertext======================
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
   const converted = parseFloat(element);
   return converted;
 };
 
-// main price update function
+//========================main price update function====================
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
@@ -53,12 +54,12 @@ const updatePrice = (id, value) => {
   document.getElementById(id).innerText = total;
 };
 
-// set innerText function
+// ====================set innerText function============================
 const setInnerText = (id, value) => {
   document.getElementById(id).innerText = parseFloat(value.toFixed(2));
 };
 
-// update delivery charge and total Tax
+// =======================update delivery charge and total Tax=================
 const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
   if (priceConverted > 200) {
@@ -74,11 +75,37 @@ const updateTaxAndCharge = () => {
     setInnerText("total-tax", priceConverted * 0.4);
   }
 };
-
-//grandTotal update function
+//=============grandTotal update function===================
 const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
   document.getElementById("total").innerText = parseFloat(grandTotal.toFixed(2));
 };
+// ==========================load details=================== 
+const loadDetails = productId =>{
+  const url = `https://fakestoreapi.com/products/${productId}`
+  fetch(url)
+  .then(res =>res.json())
+  .then(data =>showDetails(data))
+}
+// ========================show details====================
+const showDetails = product =>{
+  console.log(product)
+  const showDetailsDiv = document.getElementById('show-details')
+  showDetailsDiv.textContent = ''
+  const div = document.createElement('div')
+  div.innerHTML = `
+  <img src="${product.image}" class=" img-fluid card-img-top " alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${product.title}</h5>
+    <p> Category : ${product.category}</p>
+    <p>Information : ${product.description.slice(0,105)}.</p>
+    <p class = "fw-bold"><span class = "text-danger"><i class="fas fa-star"></i> Average Rate</span> : ${product?.rating?.rate}</p>
+    <p class = "fw-bold"><span class = "text-success"><i class="fas fa-male"></i> Count</span> : ${product?.rating?.count}</p>
+    <h3>Price: $ ${product.price}</h3>
+  </div>
+  `
+  showDetailsDiv.appendChild(div)
+  showDetailsDiv.style.display = 'block'
+}
